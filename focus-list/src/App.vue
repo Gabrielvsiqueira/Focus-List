17b4723b7af575e0cedd75dea291a57b03c27534
Gabriel Siqueira <gabriel.siqueira@globant.com>
2024-10-27 22:57:12 -0300

    feat: adicionando projeto ao repositório

--- focus-list/src/App.vue
+++ focus-list/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PageHeader></PageHeader>
+  <HomePage></HomePage>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHeader from './components/PageHeader.vue';
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage,
+    PageHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: auto;
+  background-color: #237d39;
+  font-family: 'poppins';
+  color: #FFFF;
 }
 </style>
